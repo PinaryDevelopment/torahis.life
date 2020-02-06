9efbc02f3f3bc3aa19dd601dc9995d5974f53ddd
Pinary Development LLC <hello@pinarydevelopment.com>
2020-02-06 09:36:45 -0500

    Update email text a bit.

--- src/backend/FileUploadListener/EmailerService.cs
+++ src/backend/FileUploadListener/EmailerService.cs
@@ -17,10 +17,10 @@ namespace FileUploadListener
             var client = new SendGridClient(apiKey);
             var fromEmail = emailList.First().Split(',');
             var from = new EmailAddress(fromEmail[0], fromEmail[1]);
-            var subject = $"{audioFile.Masechta} Daf {audioFile.Daf}";
+            var subject = $"{audioFile.Masechta.ToUppercaseWords()} Daf {audioFile.Daf}";
             var to = emailList.Skip(1).Select(line => line.Split(',')).Select(lineParts => new EmailAddress(lineParts[0], lineParts[1])).ToList();
-            var plainTextContent = $"{audioFile.Masechta} Daf {audioFile.Daf} is now available. https://torahis.life";
-            var htmlContent = $"<a href=\"https://torahis.life\">{audioFile.Masechta} Daf {audioFile.Daf} is now available.</a>";
+            var plainTextContent = $"{audioFile.Masechta.ToUppercaseWords()} Daf {audioFile.Daf} is now available. https://torahis.life";
+            var htmlContent = $"<a href=\"https://torahis.life\">{audioFile.Masechta} Daf {audioFile.Daf}</a> is now available.";
             var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
             //msg.AddAttachment
             var response = await client.SendEmailAsync(msg);
