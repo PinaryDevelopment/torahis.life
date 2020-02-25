ab487931db8ed4f958856bfa5a92360ce295d16c
Pinary Development LLC <hello@pinarydevelopment.com>
2020-02-25 14:33:26 -0500

    initial routing changes

---
+++ src/frontend.v2/src/components/shiurim-list.ts
@@ -0,0 +1,54 @@
+import { Shiur } from '../models/index';
+import { ShiurimService } from '../services/shiurim.service';
+
+export class ShiurimList {
+    public static parameters: string[] = ['tags'];
+
+    public shiurim: Shiur[] = [];
+    public v2: any[] = [];
+
+    public constructor(private shiurimService: ShiurimService) {
+    }
+
+    public async enter(parameters) {
+        const tags = decodeURIComponent(parameters.tags);
+        this.shiurim = await this.shiurimService.getShiurim(JSON.parse(tags));
+        const distinctDafim = this.shiurim
+            .reduce(
+                (pv: any, cv) => {
+                    const series = cv.series[1];
+                    if (pv[cv.title]) {
+                        pv[cv.title].versions.push({
+                            version: cv.series[2],
+                            duration: cv.duration,
+                            shiur: cv
+                        });
+                    } else {
+                        pv[cv.title] = {
+                            series,
+                            title: cv.title,
+                            author: cv.author,
+                            date: cv.date,
+                            versions: [
+                                {
+                                    version: cv.series[2],
+                                    duration: cv.duration,
+                                    shiur: cv
+                                }
+                            ]
+                        }
+                    }
+
+                    return pv;
+                },
+                {}
+            );
+        this.v2 = [].concat(Object.keys(distinctDafim).map(key => distinctDafim[key]))
+    }
+
+    public downloadShiur(shiur: Shiur) {
+        // window.location.href = `https://torahislife.azurewebsites.net/api/ShiurRetriever?tags=${shiur.tags.map(tag => `tags=${tag.tag}`).join('&')}`;
+        window.location.href = `http://localhost:7071/api/Shiur?title=${shiur.title}&tags=${shiur.tags.map(tag => `tags=${tag.tag}`).join('&')}`;
+    }
+
+}
---
+++ src/frontend.v2/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const cssLoader = {
+  loader: "css-loader",
+  options: {
+    modules: true,
+    // https://github.com/webpack-contrib/css-loader#importloaders
+    importLoaders: 2
+  }
+};
+
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => [
+      require('autoprefixer')()
+    ]
+  }
+};
+
+module.exports = function(env) {
+  const production = env === 'production' || process.env.NODE_ENV === 'production';
+  return {
+    mode: production ? 'production' : 'development',
+    devtool: production ? 'source-maps' : 'inline-source-map',
+    entry: './src/main.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'entry-bundle.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    devServer: {
+      historyApiFallback: true,
+      open: !process.env.CI,
+      port: 9000,
+      lazy: false
+    },
+    module: {
+      rules: [
+        { test: /\.scss$/i, use: [ "style-loader", cssLoader, postcssLoader, { loader: "sass-loader", options: { sassOptions: { includePaths: ["node_modules"] } } } ] },
+        { test: /\.ts$/i, use: ['ts-loader', '@aurelia/webpack-loader'], exclude: /node_modules/ },
+        {
+          test: /\.html$/i,
+          use: {
+            loader: '@aurelia/webpack-loader',
+            options: { useCSSModule: true }
+          },
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'index.ejs' }) //,
+      // new CopyPlugin([
+      //   // { from: 'src/assets', to: path.resolve(__dirname, '..', '..', 'docs', 'assets') },
+      //   { from: 'src/data/data.json', transform: function(content) { return JSONMinifyPlugin(content.toString()); }, to: path.resolve(__dirname, '..', '..', 'docs', 'data', '[name].[ext]') },
+      // ])
+    ]
+  }
+}
